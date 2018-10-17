6a5b2d7470b05ae8d9464b26b68d6f472bc46f1d
Damian Hickey <dhickey@gmail.com>
2018-10-17 14:40:49 +0200

    Import MS.AspNetCore.Proxy project. Rename project and namespaces.

---
+++ src/ProxyKit/HttpProxyServer.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ReverseProxyToolkit
+{
+    public class HttpProxyServer
+    {
+        public static Task RunAsync(int port = 8080)
+        {
+            return new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls($"http://localhost:{port}")
+                .Configure(a => a.Run(c => c.Response.WriteAsync("Wat")))
+                .Build()
+                .RunAsync();
+        }
+    }
+}
