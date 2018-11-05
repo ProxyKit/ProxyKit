cc035d03e7a925f56933835ec86c7f03dc17d2ad
Damian Hickey <dhickey@gmail.com>
2018-11-05 21:36:41 +0100

    Add initial build project with bullseye and simple exec

---
+++ build/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Target("default", () => System.Console.WriteLine("Hello, world!"));
+            RunTargets(args);
+        }
+    }
+}
