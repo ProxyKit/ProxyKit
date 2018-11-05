cbcc4f39dae04dc002a5e5d9bf526970a03badb5
Damian Hickey <dhickey@gmail.com>
2018-11-05 22:10:45 +0100

    Add build script.

---
+++ build.cmd
@@ -0,0 +1,2 @@
+@echo Off
+dotnet run --project build -- %*
--- build/Program.cs
+++ build/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.IO;
+using static Bullseye.Targets;
+using static SimpleExec.Command;
 
 namespace build
 {
     class Program
     {
+        private const string ArtifactsDir = "artifacts";
+        private const string Clean = "clean";
+        private const string Build = "build";
+        private const string RunTests = "run-tests";
+        private const string Pack = "pack";
+        private const string Publish = "publish";
+
         static void Main(string[] args)
         {
-            Target("default", () => System.Console.WriteLine("Hello, world!"));
+            Target(Clean, () =>
+            {
+                var di = new DirectoryInfo(ArtifactsDir);
+                foreach (var file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+
+                foreach (var dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            
+            });
+
+            Target(Build, () => Run("dotnet", "build ProxyKit.sln -c Release"));
+
+            Target(
+                RunTests,
+                DependsOn(Build),
+                () => Run("dotnet", $"test src/ProxyKit.Tests/ProxyKit.Tests.csproj -c Release -r ../../{ArtifactsDir} --no-build -l trx;LogFileName=ProxyKit.Tests.xml --verbosity=normal"));
+
+            Target(
+                Pack,
+                DependsOn(Build),
+                () => Run("dotnet", $"pack src/ProxyKit/ProxyKit.csproj -c Release -o ../../{ArtifactsDir} --no-build"));
+
+            Target(Publish, DependsOn(Pack), () =>
+            {
+                var packagesToPush = Directory.GetFiles(ArtifactsDir, "*.nupkg", SearchOption.TopDirectoryOnly);
+                Console.WriteLine($"Found packages to publish: {string.Join("; ", packagesToPush)}");
+
+                var apiKey = Environment.GetEnvironmentVariable("MYGET_API_KEY");
+
+                if (string.IsNullOrWhiteSpace(apiKey))
+                {
+                    Console.WriteLine("MyGet API key not available. Packages will not be pushed.");
+                    return;
+                }
+
+                foreach (var packageToPush in packagesToPush)
+                {
+                    Run("dotnet", $"nuget push {packageToPush} -s https://www.myget.org/F/sqlstreamstore/api/v3/index.json -k {apiKey}", true);
+                }
+            });
+
+            Target("default", DependsOn(Clean, RunTests, Publish));
+
             RunTargets(args);
         }
     }
