006ed064ba77ed7e33694b6403cb0370010e83ab
Damian Hickey <dhickey@gmail.com>
2018-11-06 14:26:42 +0100

    JWT routing example.

--- src/Examples/MultitenantJWT/Startup.cs
+++ src/Examples/MultitenantJWT/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ProxyKit.Examples.MultitenantJWT
@@ -15,11 +18,25 @@ namespace ProxyKit.Examples.MultitenantJWT
            app.RunProxy(
                requestContext =>
                {
+                   // Example of how to route a request to a backend host based on TenantId claim
+                   // in a JWT Bearer token. Note: the backend service should still token validation.
+                   // (Token validation can also be done here).
                    var authorization = requestContext.Headers.Get<string>("Authorization");
-                   if (string.IsNullOrWhiteSpace(authorization))
+                   if (string.IsNullOrWhiteSpace(authorization) || !authorization.StartsWith("Bearer"))
                    {
+                       return HttpStatusCode.Unauthorized;
                    }
-                   return requestContext.ForwardTo("http://localhost:5001");
+                   var token = authorization.Substring(0, "Bearer ".Length);
+                   var handler = new JwtSecurityTokenHandler();
+                   var jwtToken = handler.ReadJwtToken(token);
+                   var tenantIdClaim = jwtToken.Claims.SingleOrDefault(c => c.Type == "TenantId");
+
+                   if (tenantIdClaim == null)
+                   {
+                       return HttpStatusCode.Unauthorized;
+                   }
+
+                   return requestContext.ForwardTo($"http://{tenantIdClaim.Value}.internal:5001");
                },
                prepareRequestContext => prepareRequestContext.ApplyForwardedHeader());
         }
