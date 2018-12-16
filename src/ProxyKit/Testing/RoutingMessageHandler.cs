dc018d8be38f6800d65c9f8c219d70a62c7d191c
Damian Hickey <dhickey@gmail.com>
2018-12-16 09:33:50 +0100

    Use a delegating handler to call SendAsync instead of reflection.

--- src/ProxyKit/Testing/RoutingMessageHandler.cs
+++ src/ProxyKit/Testing/RoutingMessageHandler.cs
@@ -1,31 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProxyKit.Testing
 {
+    /// <summary>
+    ///     An <see cref="HttpMessageHandler"/> that acts like a router
+    ///     between multiple handlers that represent different hosts.
+    /// </summary>
     public class RoutingMessageHandler : HttpMessageHandler
     {
-        delegate Task<HttpResponseMessage> HandlerSendAsync(
-            HttpRequestMessage message,
-            CancellationToken token);
+        private readonly Dictionary<string, HostHandler> _hosts
+            = new Dictionary<string, HostHandler>();
 
-        private readonly Dictionary<string, HandlerSendAsync> _hosts
-            = new Dictionary<string, HandlerSendAsync>();
-
+        /// <summary>
+        ///     Adds a handler for a given Uri.
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="handler"></param>
         public void AddHandler(Uri uri, HttpMessageHandler handler)
         {
-            var nextDelegate = (HandlerSendAsync)
-                handler.GetType()
-                    .GetTypeInfo()
-                    .GetMethod("SendAsync", BindingFlags.NonPublic | BindingFlags.Instance)
-                    .CreateDelegate(typeof(HandlerSendAsync), handler);
-
+            var endpoint = new HostHandler(handler);
             var host = $"{uri.Host}:{uri.Port}";
-            _hosts.Add(host, nextDelegate);
+            _hosts.Add(host, endpoint);
         }
 
         protected override Task<HttpResponseMessage> SendAsync(
@@ -33,9 +32,22 @@ namespace ProxyKit.Testing
             CancellationToken cancellationToken)
         {
             var host = $"{request.RequestUri.Host}:{request.RequestUri.Port}";
-            var nextDelegate = _hosts[host];
+            var hostHandler = _hosts[host];
 
-            return nextDelegate(request, cancellationToken);
+            return hostHandler.Send(request, cancellationToken);
+        }
+
+        private class HostHandler : DelegatingHandler
+        {
+            public HostHandler(HttpMessageHandler innerHandler) 
+                : base(innerHandler)
+            {}
+
+            internal Task<HttpResponseMessage> Send(HttpRequestMessage request,
+                CancellationToken cancellationToken)
+            {
+                return SendAsync(request, cancellationToken);
+            }
         }
     }
 }
