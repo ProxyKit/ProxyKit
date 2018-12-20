806cb944bb2c5459f76911e27a7bf0abf792f7fd
Damian Hickey <dhickey@gmail.com>
2018-12-20 14:25:35 +0100

    Update tenant routing.

--- src/Examples/03_TenantRouting.cs
+++ src/Examples/03_TenantRouting.cs
@@ -1,6 +1,4 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -15,33 +13,27 @@ namespace ProxyKit.Examples
             public void ConfigureServices(IServiceCollection services)
             {
                 services.AddProxy();
+                services.AddAuthentication();
             }
 
             public void Configure(IApplicationBuilder app)
             {
+                app.UseAuthentication();
                 app.RunProxy(
                     context =>
                     {
-                        // Example of how to route a request to a backend host based on TenantId claim
-                        // in a JWT Bearer token. Note: the backend service should still token validation.
-                        // (Token validation can also be done here).
-                        var authorization = context.Request.Headers["Authorization"].SingleOrDefault();
-                        if (string.IsNullOrWhiteSpace(authorization) || !authorization.StartsWith("Bearer"))
+                        if (context.User == null)
                         {
                             return Task.FromResult(new HttpResponseMessage(HttpStatusCode.Unauthorized));
                         }
-                        var token = authorization.Substring(0, "Bearer ".Length);
-                        var handler = new JwtSecurityTokenHandler();
-                        var jwtToken = handler.ReadJwtToken(token);
-                        var tenantIdClaim = jwtToken.Claims.SingleOrDefault(c => c.Type == "TenantId");
-
-                        if (tenantIdClaim == null)
+                        var tenantId = context.User.FindFirst("TenantId")?.Value;
+                        if (tenantId == null)
                         {
                             return Task.FromResult(new HttpResponseMessage(HttpStatusCode.Unauthorized));
                         }
 
                         return context
-                            .ForwardTo($"http://{tenantIdClaim.Value}.internal:5001")
+                            .ForwardTo($"http://{tenantId}.internal:5001")
                             .Execute();
                     });
             }
