2e1ea2eb9da94802f4e7ffa12cdada3c3149aea6
Damian Hickey <dhickey@gmail.com>
2018-12-27 22:33:34 +0100

    Add stress test projects, nginx and instructions

---
+++ src/SimpleProxy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ProxyKit;
+
+namespace SimpleProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebHost
+                .CreateDefaultBuilder<Startup>(Array.Empty<string>())
+                .UseUrls("http://localhost:5001")
+                .ConfigureLogging(loggingBuilder => loggingBuilder.ClearProviders())
+                .Build()
+                .Run();
+        }
+
+        public class Startup
+        {
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddProxy();
+            }
+
+            public void Configure(IApplicationBuilder app)
+            {
+                app.RunProxy(context => context
+                    .ForwardTo("http://localhost:5002")
+                    .Execute());
+            }
+        }
+    }
+}
---
+++ src/UpstreamServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace UpstreamServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebHost
+                .CreateDefaultBuilder<Startup>(Array.Empty<string>())
+                .UseUrls("http://localhost:5002")
+                .ConfigureLogging(loggingBuilder => loggingBuilder.ClearProviders())
+                .Build()
+                .Run();
+        }
+
+        public class Startup
+        {
+            public void ConfigureServices(IServiceCollection services)
+            {}
+
+            public void Configure(IApplicationBuilder app)
+            {
+                app.Run(async context =>
+                {
+                    context.Response.ContentType = "text/plain";
+                    await context.Response.WriteAsync($"Hello, ProxyKit!\n");
+                });
+            }
+        }
+    }
+}
