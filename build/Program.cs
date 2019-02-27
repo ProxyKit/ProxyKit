8fe28803a644f0d192c74e8e1bfd15d7537d7b00
Adam Ralph <adam@adamralph.com>
2019-02-27 07:30:42 +0100

    minor build script refactoring and comments

--- build/Program.cs
+++ build/Program.cs
@@ -8,9 +8,8 @@ namespace build
     class Program
     {
         private const string ArtifactsDir = "artifacts";
-        private const string Clean = "clean";
         private const string Build = "build";
-        private const string RunTests = "run-tests";
+        private const string Test = "test";
         private const string Pack = "pack";
         private const string Publish = "publish";
 
@@ -19,7 +18,7 @@ namespace build
             Target(Build, () => Run("dotnet", "build ProxyKit.sln -c Release"));
 
             Target(
-                RunTests,
+                Test,
                 DependsOn(Build),
                 () => Run("dotnet", $"test src/ProxyKit.Tests/ProxyKit.Tests.csproj -c Release -r ../../{ArtifactsDir} --no-build -l trx;LogFileName=ProxyKit.Tests.xml --verbosity=normal"));
 
@@ -43,6 +42,7 @@ namespace build
 
                 foreach (var packageToPush in packagesToPush)
                 {
+                    // NOTE: the try catch can be removed when https://github.com/NuGet/Home/issues/1630 is released.
                     try
                     {
                         Run("dotnet", $"nuget push {packageToPush} -s https://www.myget.org/F/dh/api/v3/index.json -k {apiKey}", noEcho: true);
@@ -51,7 +51,7 @@ namespace build
                 }
             });
 
-            Target("default", DependsOn(RunTests, Publish));
+            Target("default", DependsOn(Test, Publish));
 
             RunTargetsAndExit(args);
         }
