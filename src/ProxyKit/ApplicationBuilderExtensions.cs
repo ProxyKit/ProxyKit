c09519887d2fdca65c5fecd3e270e1870331e960
Damian Hickey <dhickey@gmail.com>
2019-05-19 21:16:39 +0200

    Re-introduce the overload that takes a path and mark it as obsolete.

--- src/ProxyKit/ApplicationBuilderExtensions.cs
+++ src/ProxyKit/ApplicationBuilderExtensions.cs
@@ -33,6 +33,38 @@ namespace ProxyKit
         }
 
         /// <summary>
+        ///     Runs proxy forwarding requests to downstream server.
+        /// </summary>
+        /// <param name="app">
+        ///     The application builder.
+        /// </param>
+        /// <param name="pathMatch">
+        ///     Branches the request pipeline based on matches of the given
+        ///     request path. If the request path starts with the given path,
+        ///     the branch is executed.
+        /// </param>
+        /// <param name="handleProxyRequest">
+        ///     A delegate that can resolve the destination Uri.
+        /// </param>
+        [Obsolete("Use app.Map(\"/path\", appProxy=> { appProxy.UseProxy(...); } instead. " +
+                  "This will be removed in a future version", false)]
+        public static void RunProxy(
+            this IApplicationBuilder app,
+            PathString pathMatch,
+            HandleProxyRequest handleProxyRequest)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            app.Map(pathMatch, appInner =>
+            {
+                appInner.UseMiddleware<ProxyMiddleware>(handleProxyRequest);
+            });
+        }
+
+        /// <summary>
         ///     Adds WebSocket proxy that forwards websocket connections
         ///     to destination Uri based the HttpContext if the request matches
         ///     a given path.
