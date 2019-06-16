94c1ee3f50ba98ccac5036f658b83a64a028db0a
Damian Hickey <dhickey@gmail.com>
2019-06-16 16:01:01 +0200

    Move to more appropriate place

--- src/ProxyKit/HttpContextExtensions.cs
+++ src/ProxyKit/HttpContextExtensions.cs
@@ -1,12 +1,42 @@
 using System;
 using System.Net.Http;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ProxyKit
 {
-    internal static class HttpContextExtensions
+    public static class HttpContextExtensions
     {
-        internal static HttpRequestMessage CreateProxyHttpRequest(this HttpRequest request)
+        /// <summary>
+        ///     Forward the request to the specified upstream host.
+        /// </summary>
+        /// <param name="context">The HttpContext</param>
+        /// <param name="upstreamHost">The upstream host to forward the requests
+        /// to.</param>
+        /// <returns>A <see cref="ForwardContext"/> that represents the
+        /// forwarding request context.</returns>
+        public static ForwardContext ForwardTo(this HttpContext context, UpstreamHost upstreamHost)
+        {
+            var uri = new Uri(UriHelper.BuildAbsolute(
+                upstreamHost.Scheme,
+                upstreamHost.Host,
+                upstreamHost.PathBase,
+                context.Request.Path,
+                context.Request.QueryString));
+
+            var request = context.Request.CreateProxyHttpRequest();
+            request.Headers.Host = uri.Authority;
+            request.RequestUri = uri;
+
+            var proxyKitClient = context
+                .RequestServices
+                .GetRequiredService<ProxyKitClient>();
+
+            return new ForwardContext(context, request, proxyKitClient.Client);
+        }
+
+        private static HttpRequestMessage CreateProxyHttpRequest(this HttpRequest request)
         {
             var requestMessage = new HttpRequestMessage();
             var requestMethod = request.Method;
