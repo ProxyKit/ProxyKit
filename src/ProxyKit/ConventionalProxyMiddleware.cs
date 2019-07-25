fdfcb9a3b95e13c644cf810af7b2aedcebefaf21
Aaron Job <aaron@gravypower.net>
2019-07-25 15:29:13 +1000

    Added factory activated ProxyMiddleware

---
+++ src/ProxyKit/ConventionalProxyMiddleware.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ProxyKit
+{
+    public class ConventionalProxyMiddleware<TProxyHandler> : ProxyMiddleware
+        where TProxyHandler:IProxyHandler
+    {
+        private readonly TProxyHandler _handler;
+
+        public ConventionalProxyMiddleware(RequestDelegate _, TProxyHandler handler)
+        {
+            _handler = handler;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            using (var response = await _handler.HandleProxyRequest(context).ConfigureAwait(false))
+            {
+                await CopyProxyHttpResponse(context, response).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ src/ProxyKit/FactoryActivatedProxyMiddleware.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ProxyKit
+{
+    public class FactoryActivatedProxyMiddleware<TProxyHandler> : ProxyMiddleware, IMiddleware
+        where TProxyHandler : IProxyHandler
+    {
+        private readonly TProxyHandler _handler;
+
+        public FactoryActivatedProxyMiddleware(TProxyHandler handler)
+        {
+            _handler = handler;
+        }
+        
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            using (var response = await _handler.HandleProxyRequest(context).ConfigureAwait(false))
+            {
+                await CopyProxyHttpResponse(context, response).ConfigureAwait(false);
+            }
+        }
+    }
+}
