d7bd241db4db6a570326960940bb0bf33231ed26
Damian Hickey <dhickey@gmail.com>
2019-10-01 19:05:31 +0200

    Dont run with a build-arg...

--- build.cmd
+++ build.cmd
@@ -12,6 +12,7 @@ if errorlevel 1 (
 docker run --rm --name proxykit-build ^
  -v %cd%/artifacts:/repo/artifacts ^
  -v %cd%/.git:/.git ^
+ -e MYGET_API_KEY=$MYGET_API_KEY ^
  proxykit-build ^
  dotnet run -p /repo/build/build.csproj -c Release -- %*
 
--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ docker build \
  --tag proxykit-build .
 
 docker run --rm --name proxykit-build \
- --build-arg MYGET_API_KEY=$MYGET_API_KEY \
  -v $PWD/artifacts:/repo/artifacts \
  -v $PWD/.git:/repo/.git \
+ -e MYGET_API_KEY=$MYGET_API_KEY \
  proxykit-build \
  dotnet run -p /repo/build/build.csproj -c Release -- "$@"
