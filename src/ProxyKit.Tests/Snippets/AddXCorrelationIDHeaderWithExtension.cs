056e5b3b5c127e0066c361bc3967b19d340d935d
SimonCropp <simon.cropp@gmail.com>
2020-01-09 21:07:31 +1100

    doco proposal

---
+++ src/ProxyKit.Tests/Snippets/AddXCorrelationIDHeaderWithExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using ProxyKit;
+
+public class AddXCorrelationIDHeaderWithExtension
+{
+    #region AddXCorrelationIDHeaderWithExtension
+    public void Configure(IApplicationBuilder app)
+    {
+        app.RunProxy(context => context
+            .ForwardTo("http://upstream-server:5001/")
+            .ApplyCorrelationId()
+            .Send());
+    }
+    #endregion
+}
