09505c720fcae06fb08c4e50ddb23ddcd7fbabc3
Damian Hickey <dhickey@gmail.com>
2020-02-16 17:41:42 +0100

    Content length may not be supplied so check if the body can be read. Not sure if this is good for all scenarios. yet

--- src/ProxyKit/HttpContextExtensions.cs
+++ src/ProxyKit/HttpContextExtensions.cs
@@ -52,8 +52,9 @@ namespace ProxyKit
         {
             var requestMessage = new HttpRequestMessage();
             
+            Console.WriteLine(request.ContentLength.HasValue);
             //Only copy Body when original request has a body.
-            if (request.ContentLength.HasValue)
+            if (request.Body.CanRead)
             {
                 var streamContent = new StreamContent(request.Body);
                 requestMessage.Content = streamContent;
