303e171f81a8456cce3b63380fceb06d84dd2dfa
Damian Hickey <dhickey@gmail.com>
2020-05-01 13:45:05 +0200

    Set BUILD_NUMBER environment to GITHUB_RUN_NUMBER

--- build.cmd
+++ build.cmd
@@ -4,6 +4,7 @@ docker run --rm -it --name proxykit-build ^
  -v %cd%:/repo ^
  -w /repo ^
  -e FEEDZ_PROXYKIT_API_KEY=%FEEDZ_PROXYKIT_API_KEY% ^
+ -e BUILD_NUMBER=%GITHUB_RUN_NUMBER% ^
  damianh/dotnet-core-lts-sdks ^
  dotnet run -p build/build.csproj -c Release -- %*
 
--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ docker run --rm --name proxykit-build \
  -v $PWD:/repo \
  -w /repo \
  -e FEEDZ_PROXYKIT_API_KEY=$FEEDZ_PROXYKIT_API_KEY \
+ -e BUILD_NUMBER=$GITHUB_RUN_NUMBER \
  damianh/dotnet-core-lts-sdks \
  dotnet run -p build/build.csproj -c Release -- "$@"
