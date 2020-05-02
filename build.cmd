d6b64566868cf8fc10adef949479d56112def61f
Damian Hickey <dhickey@gmail.com>
2020-05-02 13:17:52 +0200

    Specify the tag
    # Conflicts:
    #	build.cmd
    #	build.sh

--- build.cmd
+++ build.cmd
@@ -4,7 +4,8 @@ docker run --rm -it --name proxykit-build ^
  -v %cd%:/repo ^
  -w /repo ^
  -e FEEDZ_PROXYKIT_API_KEY=%FEEDZ_PROXYKIT_API_KEY% ^
- damianh/dotnet-core-lts-sdks ^
+ -e BUILD_NUMBER=%GITHUB_RUN_NUMBER% ^
+ damianh/dotnet-core-lts-sdks:1 ^
  dotnet run -p build/build.csproj -c Release -- %*
 
 if errorlevel 1 (
--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ docker run --rm --name proxykit-build \
  -v $PWD:/repo \
  -w /repo \
  -e FEEDZ_PROXYKIT_API_KEY=$FEEDZ_PROXYKIT_API_KEY \
- damianh/dotnet-core-lts-sdks \
+ -e BUILD_NUMBER=$GITHUB_RUN_NUMBER \
+ damianh/dotnet-core-lts-sdks:1 \
  dotnet run -p build/build.csproj -c Release -- "$@"
